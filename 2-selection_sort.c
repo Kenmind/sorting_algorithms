59d616bfc91437640f2d06ce4515012a1f45a117
Kennedy Kalaluka <kennedykalaluka04@gmail.com>
2022-01-07 11:46:46 +0000

    selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+
+/**
+ * swap - swaps integers
+ * @x: Pointer to first integer
+ * @y: Pointer to second integer
+ */
+
+void swap(int *x, int *y)
+{
+	int temp = *x;
+	*x = *y;
+	*y = temp;
+}
+
+/**
+ * selection_sort - sorts an array using slection sort algorithm
+ * @array: Array to be sorted
+ * @size: size of the array
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min_idx;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min_idx = i;
+		for (j = i + 1; j < size; j++)
+			if (array[j] < array[min_idx])
+				min_idx = j;
+		swap(&array[min_idx], &array[i]);
+	}
+}
