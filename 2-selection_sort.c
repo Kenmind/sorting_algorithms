ba9ebe81773b118115af3344dc4409e03caec349
Kennedy Kalaluka <kennedykalaluka04@gmail.com>
2022-01-12 08:54:46 +0000

    corrected selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -4,17 +4,15 @@
  * swap - swaps integers and prints the array
  * @x: Pointer to first integer
  * @y: Pointer to second integer
- * @arr: Array to print
- * @size: size of array arr
+ *
  */
 
-void swap(int *x, int *y, int *arr, size_t size)
+void swap(int *x, int *y)
 {
 	int temp = *x;
 
 	*x = *y;
 	*y = temp;
-	print_array(arr, size);
 }
 
 /**
@@ -27,12 +25,15 @@ void selection_sort(int *array, size_t size)
 {
 	size_t i, j, min_idx;
 
+	if (!array || size < 2)
+		return;
 	for (i = 0; i < size - 1; i++)
 	{
 		min_idx = i;
 		for (j = i + 1; j < size; j++)
 			if (array[j] < array[min_idx])
 				min_idx = j;
-		swap(&array[min_idx], &array[i], array, size);
+		swap(array + min_idx, array + i);
+		print_array(array, size);
 	}
 }
